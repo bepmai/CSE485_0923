ce1f02eb1c5d46e8242051e6563bc416494f2059
bepmai <nguyenphgmaiii23@gmail.com>
2023-09-22 22:39:19 +0700

    Chinh sua

--- BT02/blogs/delete_table_blogs.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $sql = "TRUNCATE TABLE blogs";
-    $query = mysqli_query($connect, $sql);
-    header('location: index_blogs.php?page_layout1=list_blogs');
-?>
--- BT02/user/delete_table.php
+++
@@ -1,5 +0,0 @@
-<?php
-    $sql = "TRUNCATE TABLE users";
-    $query = mysqli_query($connect, $sql);
-    header('location: index.php?page_layout=list');
-?>
