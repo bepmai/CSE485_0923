68bcc84df52ae9e4a400cfe0c025d7c31daf84bb
bepmai <nguyenphgmaiii23@gmail.com>
2023-09-25 21:42:38 +0700

    Them

--- BT02/user/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $id = $_GET['id'];
-    $sql = "DELETE from users where idusers = $id";
-    $query = mysqli_query($connect, $sql);
-    header('location: index.php?page_layout=list');
-?>
