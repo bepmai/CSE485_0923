43fbe3d597756e294dea238abb3885655f8e4f0e
bepmai <nguyenphgmaiii23@gmail.com>
2023-09-30 19:08:41 +0700

    Sua

--- BT02/add/menu-child.php
+++
@@ -1,4 +0,0 @@
-<div><i class="bi bi-gear-fill icon-right"></i></div>
-<div><i class="bi bi-envelope-fill icon-right"></i></div>
-<div><i class="bi bi-bell-fill icon-right"></i></div>
-<div class="nation"><img src="./img/eng.png" class="nation-eng"><br><span class="text-eng">eng</span></div>
--- BTTH01B_CSE485_ex01/config/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-    $connect = mysqli_connect('localhost', 'root','','btth01_cse485_ex02');
-    if($connect){
-        mysqli_query($connect, "set names 'UTF8'");
-    }
-    else{
-        echo "ket noi that bai";
-    }
-?>
