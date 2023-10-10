3d4bc16a785980b71259b7087e6b8e7266c92429
bepmai <nguyenphgmaiii23@gmail.com>
2023-10-10 23:42:28 +0700

    edit

--- BTTH01B/b02/admin.php
+++ BTTH01B/b02/admin.php
@@ -1,22 +1,26 @@
 <?php
     $sql_SumUsers = "SELECT COUNT(*) as Sum_users FROM users";
-    $query_SumUsers = mysqli_query($connect, $sql_SumUsers);
-    $row_Users = mysqli_fetch_assoc($query_SumUsers);
+    $query_SumUsers = $conn->prepare($sql_SumUsers);
+    $query_SumUsers->execute();
+    $row_Users = $query_SumUsers->fetch();
     $SumUsers = $row_Users['Sum_users'];
-   
+
     $sql_SumTloai = "SELECT COUNT(*) as Sum_Tloai FROM theloai";
-    $query_SumTloai = mysqli_query($connect, $sql_SumTloai);
-    $row_Tloai = mysqli_fetch_assoc($query_SumTloai);
+    $query_SumTloai = $conn->prepare($sql_SumTloai);
+    $query_SumTloai->execute();
+    $row_Tloai = $query_SumTloai->fetch();
     $SumTloai = $row_Tloai['Sum_Tloai'];
 
     $sql_SumTgia = "SELECT COUNT(*) as Sum_Tgia FROM tacgia";
-    $query_SumTgia = mysqli_query($connect, $sql_SumTgia);
-    $row_Tgia = mysqli_fetch_assoc($query_SumTgia);
+    $query_SumTgia = $conn->prepare($sql_SumTgia);
+    $query_SumTgia->execute();
+    $row_Tgia = $query_SumTgia->fetch();
     $SumTgia = $row_Tgia['Sum_Tgia'];
 
     $sql_SumBV = "SELECT COUNT(*) as Sum_BV FROM baiviet";
-    $query_SumBV = mysqli_query($connect, $sql_SumBV);
-    $row_BV = mysqli_fetch_assoc($query_SumBV);
+    $query_SumBV = $conn->prepare($sql_SumBV);
+    $query_SumBV->execute();
+    $row_BV = $query_SumBV->fetch();
     $SumBV = $row_BV['Sum_BV'];
 ?>
 <div class="container-fluid" style="height: 653px;">
@@ -30,7 +34,7 @@
         <div class="col-md-10">
             <div class="row d-flex">
                 <div class="col-md-3">
-                    <a class="link-offset-2 link-underline link-underline-opacity-0" href="">&emsp;Người dùng</a><br><br>
+                    <a class="link-offset-2 link-underline link-underline-opacity-0" href="index_admin.php?page_layout_admin=table_users">&emsp;Người dùng</a><br><br>
                     <p style="border:1px solid gray; height: 130px; width: 130px">
                         <?php echo '<sapn style = "font-weight:600; font-size: 40px; position: relative;top: 25px;left: 50px">'.$SumUsers.'</sapn>';?>
                     </p>
--- BTTH03/app/config/config.php
+++
@@ -1,4 +0,0 @@
-<?php
-define('APP_ROOT', dirname(__FILE__, 3));
-define('DOMAIN','http://localhost/TH_PHP/BTTH03');
-?>
